62024b7709a1d77dc908b1f8fe214b9e5dfa05a9
jrdanfox <fox.1061@osu.edu>
2019-06-08 17:41:18 -0400

    initial commit, added User and SlackUser classes

---
+++ src/integration/SlackUser.java
@@ -0,0 +1,8 @@
+package integration;
+
+public class SlackUser extends User {
+
+	public SlackUser(String userName) {
+		super(userName);
+	}
+}
---
+++ src/integration/User.java
@@ -0,0 +1,14 @@
+package integration;
+
+public class User {
+	
+	private String userName;
+	
+	public User(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+}
