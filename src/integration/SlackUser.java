26cada2dbc2395818b3edd2d1b7d61f7ffe883c5
jrdanfox <fox.1061@osu.edu>
2019-06-09 18:16:55 -0400

    added some fields to SlackUser

--- src/integration/SlackUser.java
+++ src/integration/SlackUser.java
@@ -2,7 +2,17 @@ package integration;
 
 public class SlackUser extends User {
 
-	public SlackUser(String userName) {
-		super(userName);
+	private String team_id;
+	private boolean deleted;
+	private String real_name;
+	private String[] profile;
+	private boolean is_admin;
+	private boolean is_owner;
+	private boolean is_bot;
+	private int updated;
+	
+	public SlackUser(String userName, String userID) {
+		super(userName, userID);
 	}
+	
 }
--- src/integration/User.java
+++ src/integration/User.java
@@ -3,12 +3,18 @@ package integration;
 public class User {
 	
 	private String userName;
+	private String userID;
 	
-	public User(String userName) {
+	public User(String userName, String userID) {
 		this.userName = userName;
+		this.userID = userID;
 	}
 	
 	public String getUserName() {
 		return this.userName;
+	}
+	
+	public String getUserID() {
+		return this.userID;
 	}
 }
