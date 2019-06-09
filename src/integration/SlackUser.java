894f251897684a045012f6c535520be8a78f9825
Jordan Fox <fox.1061@osu.edu>
2019-06-09 18:36:07 -0400

    added getters for SlackUser

--- src/integration/SlackUser.java
+++ src/integration/SlackUser.java
@@ -11,8 +11,50 @@ public class SlackUser extends User {
 	private boolean is_bot;
 	private int updated;
 	
-	public SlackUser(String userName, String userID) {
+	public SlackUser(String userName, String userID, String team_id, boolean deleted,
+			String real_name, String[] profile, boolean is_admin, boolean is_owner,
+			boolean is_bot, int updated) {
 		super(userName, userID);
+		this.team_id = team_id;
+		this.deleted = deleted;
+		this.real_name = real_name;
+		this.profile = profile;
+		this.is_admin = is_admin;
+		this.is_owner = is_owner;
+		this.is_bot = is_bot;
+		this.updated = updated;
+	}
+	
+	public String getTeam_id() {
+		return team_id;
+	}
+
+	public boolean getDeleted() {
+		return deleted;
+	}
+	
+	public String getReal_name() {
+		return real_name;
+	}
+	
+	public  String[] getProfile() {
+		return profile;
+	}
+	
+	public boolean getIs_admin() {
+		return  is_admin;
+	}
+	
+	public boolean getIs_owner() {
+		return is_owner;
+	}
+	
+	public boolean getIs_bot() {
+		return is_bot;
+	}
+	
+	public int getUpdated()  {
+		return updated;
 	}
 	
 }
